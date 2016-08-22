20ea8089f8a663f12a94c6ddd1b0c0da852073ca
Eric and Beth Hayes <EricandBethHayes@erics-imac.local>
2016-08-21 22:02:31 -0600

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Eric and Beth Hayes on 8/21/16.
+//  Copyright © 2016 Eric and Beth Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
